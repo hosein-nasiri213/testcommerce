f59db59e79365756b0b2a8839218fcc8c40d37d9
hosein-nasiri213 <hoseinmnasiri213@gmail.com>
2023-03-14 18:25:50 +0330

    product-1

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,21 +1,117 @@
-import React, {useState , useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 
 const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setfilter] = useState(data);
+  const [loading, setloading] = useState(false);
+  let componentMounted = true;
 
-    const [data, setData] = useState([]);
-    const [filter, setfilter] = useState(data);
-    const [loading, setloading] = useState(false);
-    let componentMounted = true ;
+  useEffect(() => {
+    const getProducts = async () => {
+      setloading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setfilter(await response.json());
+        setloading(false);
+        console.log(filter);
+      }
 
-    useEffect(() => {
-        getProducts();
-    }, [input])
-    
+      return () => {
+        componentMounted = false;
+      };
+    };
 
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+      </>
+    );
+  };
+
+  const filterProduct = (cat) => {
+    const updatedList = data.filter((x)=>x.category === cat);
+  }
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(data)}>
+            All
+          </button>
+          <button className="btn btn-outline-dark me-2" onClick={() =>filterProduct("men's clothing")}>
+            Men's clothing
+          </button>
+          <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct("women's clothing")}>
+            Women's clothing
+          </button>
+          <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct("jewelery")}>
+            Jwelery</button>
+          <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct("electronic")}>Electronic</button>
+        </div>
+        {filter.map((product) => {
+          return (
+            <>
+              <div className="col-md-3 mb-4">
+                <div class="card h-100 text-center p-4">
+                  <img
+                    src={product.image}
+                    class="card-img-top"
+                    alt={product.title}
+                    height="250px"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title mb-0">
+                      {product.title.substring(0, 12)}...
+                    </h5>
+                    <p class="card-text lead fw-bold">${product.price}</p>
+                    <a href="#" class="btn btn-outline-dark">
+                      buy now
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </>
+    );
+  };
 
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center font-aviny">
+              محصولات عمو سعا
+            </h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
